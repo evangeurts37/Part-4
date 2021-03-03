c902d60a01399893da6cb264be22990d10af65ec
Evan Geurts <GeurEvan189@GoTVDSB.ca>
2021-03-03 10:54:13 -0500

    set up done

--- Random Numbers/Form1.Designer.cs
+++ Random Numbers/Form1.Designer.cs
@@ -28,97 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblInstruction = new System.Windows.Forms.Label();
+            this.btnInt = new System.Windows.Forms.Button();
+            this.btnDouble = new System.Windows.Forms.Button();
+            this.lblMin = new System.Windows.Forms.Label();
+            this.lblMax = new System.Windows.Forms.Label();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.txtMin = new System.Windows.Forms.TextBox();
+            this.txtMax = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // label1
+            // lblInstruction
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(78, 33);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.lblInstruction.AutoSize = true;
+            this.lblInstruction.Location = new System.Drawing.Point(78, 33);
+            this.lblInstruction.Name = "lblInstruction";
+            this.lblInstruction.Size = new System.Drawing.Size(197, 13);
+            this.lblInstruction.TabIndex = 0;
+            this.lblInstruction.Text = "Please enter a maximum and a minimum:";
             // 
-            // button1
+            // btnInt
             // 
-            this.button1.Location = new System.Drawing.Point(49, 171);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnInt.Location = new System.Drawing.Point(49, 171);
+            this.btnInt.Name = "btnInt";
+            this.btnInt.Size = new System.Drawing.Size(75, 23);
+            this.btnInt.TabIndex = 1;
+            this.btnInt.Text = "Integer";
+            this.btnInt.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnDouble
             // 
-            this.button2.Location = new System.Drawing.Point(183, 181);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnDouble.Location = new System.Drawing.Point(183, 181);
+            this.btnDouble.Name = "btnDouble";
+            this.btnDouble.Size = new System.Drawing.Size(75, 23);
+            this.btnDouble.TabIndex = 2;
+            this.btnDouble.Text = "Double";
+            this.btnDouble.UseVisualStyleBackColor = true;
             // 
-            // label2
+            // lblMin
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(46, 86);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "label2";
+            this.lblMin.AutoSize = true;
+            this.lblMin.Location = new System.Drawing.Point(46, 86);
+            this.lblMin.Name = "lblMin";
+            this.lblMin.Size = new System.Drawing.Size(48, 13);
+            this.lblMin.TabIndex = 3;
+            this.lblMin.Text = "Minimum";
             // 
-            // label3
+            // lblMax
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(49, 124);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "label3";
+            this.lblMax.AutoSize = true;
+            this.lblMax.Location = new System.Drawing.Point(49, 124);
+            this.lblMax.Name = "lblMax";
+            this.lblMax.Size = new System.Drawing.Size(51, 13);
+            this.lblMax.TabIndex = 4;
+            this.lblMax.Text = "Maximum";
             // 
-            // label4
+            // lblAnswer
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(102, 225);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
-            this.label4.TabIndex = 5;
-            this.label4.Text = "label4";
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Location = new System.Drawing.Point(102, 225);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(49, 13);
+            this.lblAnswer.TabIndex = 5;
+            this.lblAnswer.Text = "message";
+            this.lblAnswer.UseMnemonic = false;
             // 
-            // textBox1
+            // txtMin
             // 
-            this.textBox1.Location = new System.Drawing.Point(133, 86);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 6;
+            this.txtMin.Location = new System.Drawing.Point(133, 86);
+            this.txtMin.Name = "txtMin";
+            this.txtMin.Size = new System.Drawing.Size(100, 20);
+            this.txtMin.TabIndex = 6;
             // 
-            // textBox2
+            // txtMax
             // 
-            this.textBox2.Location = new System.Drawing.Point(133, 124);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 7;
+            this.txtMax.Location = new System.Drawing.Point(133, 124);
+            this.txtMax.Name = "txtMax";
+            this.txtMax.Size = new System.Drawing.Size(100, 20);
+            this.txtMax.TabIndex = 7;
             // 
             // RandomNumbers
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(377, 280);
+            this.Controls.Add(this.txtMax);
+            this.Controls.Add(this.txtMin);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.lblMax);
+            this.Controls.Add(this.lblMin);
+            this.Controls.Add(this.btnDouble);
+            this.Controls.Add(this.btnInt);
+            this.Controls.Add(this.lblInstruction);
             this.Name = "RandomNumbers";
             this.Text = "Random Numbers";
             this.ResumeLayout(false);
@@ -128,14 +129,14 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lblInstruction;
+        private System.Windows.Forms.Button btnInt;
+        private System.Windows.Forms.Button btnDouble;
+        private System.Windows.Forms.Label lblMin;
+        private System.Windows.Forms.Label lblMax;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.TextBox txtMin;
+        private System.Windows.Forms.TextBox txtMax;
     }
 }
 
